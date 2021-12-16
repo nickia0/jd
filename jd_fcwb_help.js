6da53fc4fc34c09a3324d47311d8e32383ba0628
Argon <75816501+Argon-av@users.noreply.github.com>
2021-12-16 15:11:17 +0800

    发财挖宝助力

--- jd_fcwb_help.js
+++ jd_fcwb_help.js
@@ -37,7 +37,7 @@ let curRound = 1
     console.log(`\n注意：本脚本暂时只会执行助力，助力后，请手动进活动进行游戏（发财挖宝: 入口,极速版-》我的-》发财挖宝）\n`)
     let res = [];
 
-    try{res = await getAuthorShareCode('https://ghproxy.com/https://raw.githubusercontent.com/shufflewzc/updateTeam/main/shareCodes/fcwb.json');}catch (e) {}
+    try{res = await getAuthorShareCode('https://ghproxy.com/https://raw.githubusercontent.com/Argon-av/backup/main/code/fcwb.json');}catch (e) {}
      if(!res){res = [];}
     
     if(res.length > 0){
