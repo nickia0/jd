63ff93eda06dc78debdfec0412261f413b6cd363
Argon <75816501+Argon-av@users.noreply.github.com>
2022-01-14 09:47:02 +0800

    Update jd_fanli.js

--- jd_fanli.js
+++ jd_fanli.js
@@ -4,7 +4,7 @@
 活动入口：https://u.jd.com/ytWx4w0
 * 9,17 * * * jd_fanli.js
  */
-const $ = new Env('搞基大神-饭粒');
+const $ = new Env('京东饭粒');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
