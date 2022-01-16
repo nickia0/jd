4128c387444086bce4504c306e7b9140adde9b87
Argon <75816501+Argon-av@users.noreply.github.com>
2022-01-16 09:14:35 +0800

    Update jd_babel_sign.js

--- jd_babel_sign.js
+++ jd_babel_sign.js
@@ -1,7 +1,6 @@
 /**
-通天塔签到共建,签到类cron自行设置.
+0 0,3,10 * * * jd_babel_sign.js
 入口：主页-秒杀-狂撒三亿京豆
-1 0,8 * * * jd_babel_sign.js
 TG频道：https://t.me/sheeplost
 */
 const $ = new Env('通天塔签到共建');
@@ -61,7 +60,9 @@ async function main() {
             let taskInfo = await task('doInteractiveAssignment', { "encryptProjectId": e.encryptProjectId, "encryptAssignmentId": e.encryptAssignmentId, "completionFlag": true, "itemId": "1", "sourceCode": "aceaceqingzhan" });
             if (taskInfo.code === "0" && taskInfo.subCode === "0") {
                 console.log(JSON.stringify(taskInfo.rewardsInfo.successRewards));
-            } else { console.log('没有获取到活动ID!') }
+            } else { console.log(JSON.stringify(taskInfo)) }
+        } else {
+            console.log('没有获取到活动ID')
         }
     } else { console.log('没有获取到活动信息！') };
 }
