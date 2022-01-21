2dbdb2e449dc3cd5c0fa133c13bd8208956d1d28
Argon <75816501+Argon-av@users.noreply.github.com>
2022-01-21 09:33:30 +0800

    Update jd_gold_sign.js

--- jd_gold_sign.js
+++ jd_gold_sign.js
@@ -1,7 +1,16 @@
 /*
 京东金榜
-13 6,13 * * * jd_gold_sign.js
-*/
+活动入口：https://h5.m.jd.com/babelDiy/Zeus/2H5Ng86mUJLXToEo57qWkJkjFPxw/index.html
+by:小手冰凉 tg:@chianPLA
+脚本更新时间：2022-1-5
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+新手写脚本，难免有bug，能用且用。
+===================quantumultx================
+[task_local]
+#京东金榜
+13 7,9 * * * jd_gold_sign.js
+
+ */
 const $ = new Env('京东金榜');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
